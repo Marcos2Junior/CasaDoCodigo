854f2bb89679366514654685150c474ae132ca7a
Marcos Junior <Mod.gluguer@gmail.com>
2021-07-01 10:40:06 -0300

    Funcionalidade #1; adicionado repositorio, interface para nova entidade categoria;

---
+++ CasaDoCodigo/Repositories/CategoriaRepository.cs
@@ -0,0 +1,30 @@
+﻿using CasaDoCodigo.Models;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CasaDoCodigo.Repositories
+{
+    public interface ICategoriaRepository
+    {
+        Task<Categoria> GravaCategoriaAsync(string nome);
+    }
+    public class CategoriaRepository : BaseRepository<Categoria>, ICategoriaRepository
+    {
+        public CategoriaRepository(ApplicationContext contexto) : base(contexto)
+        {
+        }
+
+        public async Task<Categoria> GravaCategoriaAsync(string nome)
+        {
+            Categoria categoria = dbSet.FirstOrDefault(x => x.Nome == nome);
+            if (categoria == null)
+            {
+                categoria = new Categoria(nome);
+                contexto.Add(categoria);
+                await contexto.SaveChangesAsync();
+            }
+
+            return categoria;
+        }
+    }
+}
